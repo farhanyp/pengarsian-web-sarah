1bcd12fe4853ff3c5ab2db8c4d84d6c1d2fa3cd8
farhanyp <farhan.yudha2016@gmail.com>
2026-05-23 20:35:21 +0700

    feat: initialize user authentication, role management, and font configuration

--- app/Enums/RoleType.php
+++ app/Enums/RoleType.php
@@ -5,6 +5,7 @@ namespace App\Enums;
 enum RoleType: string
 {
     case SUPERADMIN = 'SUPERADMIN';
+    case ADMIN = 'ADMIN';
     case GURU = 'GURU';
     case KEPALA_SEKOLAH = 'KEPALA_SEKOLAH';
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Enums\RoleType;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,13 +15,51 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            RoleAndPermissionSeeder::class,
-            UserSeeder::class,
-            StudentSeeder::class,
-            SubjectSeeder::class,
-            StudentGradeSeeder::class,
-            DocumentSeeder::class,
-        ]);
+        // 1. Create Roles
+        $superadminRole = Role::firstOrCreate(['name' => RoleType::SUPERADMIN->value, 'guard_name' => 'web']);
+        $adminRole = Role::firstOrCreate(['name' => RoleType::ADMIN->value, 'guard_name' => 'web']);
+        $guruRole = Role::firstOrCreate(['name' => RoleType::GURU->value, 'guard_name' => 'web']);
+        $kepsekRole = Role::firstOrCreate(['name' => RoleType::KEPALA_SEKOLAH->value, 'guard_name' => 'web']);
+
+        // 2. Create Users for each role
+        // a. User Superadmin (Developer/Shadow Role)
+        $superadminUser = User::firstOrCreate(
+            ['email' => 'developer@scholarsys.edu'],
+            [
+                'name' => 'Superadmin Developer',
+                'password' => Hash::make('password'),
+            ]
+        );
+        $superadminUser->assignRole($superadminRole);
+
+        // b. User Admin
+        $adminUser = User::firstOrCreate(
+            ['email' => 'admin@scholarsys.edu'],
+            [
+                'name' => 'Admin System',
+                'password' => Hash::make('password'),
+            ]
+        );
+        $adminUser->assignRole($adminRole);
+
+        // c. User Guru
+        $guruUser = User::firstOrCreate(
+            ['email' => 'guru@scholarsys.edu'],
+            [
+                'name' => 'Guru Pengajar',
+                'password' => Hash::make('password'),
+            ]
+        );
+        $guruUser->assignRole($guruRole);
+
+        // d. User Kepala Sekolah
+        $kepsekUser = User::firstOrCreate(
+            ['email' => 'kepsek@scholarsys.edu'],
+            [
+                'name' => 'Kepala Sekolah',
+                'password' => Hash::make('password'),
+            ]
+        );
+        $kepsekUser->assignRole($kepsekRole);
     }
 }
